adb0bc71b7663bf0eb9f307e7d69257f57fc4508
Rajasegar Chandran <rajasegar.c@gmail.com>
2022-07-28 16:22:30 +0530

    chore: adding login and register pages

--- src/web.lisp
+++ src/web.lisp
@@ -20,6 +20,11 @@
 (defvar *web* (make-instance '<web>))
 (clear-routing-rules *web*)
 
+(defun get-param (name parsed)
+  "Get parameter values from _parsed"
+  (cdr (assoc name parsed :test #'string=)))
+
+
 ;;
 ;; Routing rules
 
@@ -28,6 +33,16 @@
 
 (defroute "/login" ()
   (render #P"login.html"))
+
+(defroute ("/login" :method "POST") (&key _parsed)
+  (print _parsed)
+  (let ((username (get-param "username" _parsed))
+    (password (get-param "pwd" _parsed)))
+    
+  (render #P"index.html" (list :authenticated T))))
+
+(defroute "/register" ()
+  (render #P"register.html"))
 
 ;; Error pages
 
